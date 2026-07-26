658901eecc412c0465060ab2e408e4342a98461c
ahsan rehman <ahsan.rehman@ropstam.com>
2026-07-26 19:48:22 +0500

    merge intervals meeting-rooms

---
+++ mergeIntervals/meeting-rooms.js
@@ -0,0 +1,38 @@
+function minMeetingRooms(start, end) {
+  start = start.sort((a, b) => a - b);
+  end = end.sort((a, b) => a - b);
+  let left = 0;
+  let right = 0;
+  let rooms = 0;
+  let max = -Infinity;
+  while (left < start.length && right < end.length) {
+    if (start[left] < end[right]) {
+      rooms++;
+      max = Math.max(rooms, max);
+      left++;
+    } else {
+      rooms--;
+      right++;
+    }
+  }
+  while (left < start.length) {
+    rooms++;
+    max = Math.max(rooms, max);
+    left++;
+  }
+  while (right < end.length) {
+    rooms--;
+    right++;
+  }
+  return max;
+}
+
+// const start = [10, 22, 11, 4, 11];
+// const end = [14, 29, 30, 12, 29];
+const start = [1, 10, 7];
+const end = [4, 15, 10];
+/*
+start = [2, 9, 6], end = [4, 12, 10]
+start = [2, 6, 9], end = [4, 10, 12]
+*/
+console.log(minMeetingRooms(start, end));
